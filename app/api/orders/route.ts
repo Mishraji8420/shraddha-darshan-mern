b13923342dbd63c75dde5a6f10f3bcf622552b81
MIshraji8420 <shivammishra8420@gmail.com>
2026-08-02 23:14:43 +0530

    feat: secure order api using nextauth session

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,12 +1,28 @@
+import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 export async function POST(req: Request) {
   try {
+    // Get logged-in user
+    const session = await auth();
+
+    if (!session?.user?.id) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Unauthorized",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    // Read request body
     const body = await req.json();
 
     const {
-      userId,
       address,
       items,
       subtotal,
@@ -14,19 +30,28 @@ export async function POST(req: Request) {
       total,
     } = body;
 
-    if (!userId || !items?.length) {
+    // Validation
+    if (!items || items.length === 0) {
       return NextResponse.json(
-        { message: "Invalid order data" },
-        { status: 400 }
+        {
+          success: false,
+          message: "Cart is empty",
+        },
+        {
+          status: 400,
+        }
       );
     }
 
+    // Create Order
     const order = await prisma.order.create({
       data: {
-        userId,
+        userId: session.user.id,
+
         subtotal,
         shipping,
         total,
+
         status: "PENDING",
         paymentStatus: "PENDING",
 
@@ -48,9 +73,9 @@ export async function POST(req: Request) {
 
     return NextResponse.json({
       success: true,
+      message: "Order created successfully",
       order,
     });
-
   } catch (error) {
     console.error(error);
 
